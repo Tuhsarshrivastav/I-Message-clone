763fe6be49786573085d4ac754a4f8e18db85d1b
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-02-27 18:46:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyAtEP6g5R8izOrQKPf735egaxAq8gIvw2o",
-  authDomain: "i-message-clone-5bf6a.firebaseapp.com",
-  projectId: "i-message-clone-5bf6a",
-  storageBucket: "i-message-clone-5bf6a.appspot.com",
-  messagingSenderId: "937063372834",
-  appId: "1:937063372834:web:67eb0b049ee008c13ed27b",
-  measurementId: "G-HEX5WN0WZD",
+//  copy your firebase cdn 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
